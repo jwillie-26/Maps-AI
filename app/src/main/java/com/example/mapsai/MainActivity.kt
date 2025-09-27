fb3f241011c82c2f13ab755a9e2c6342fab16df8
Jesse Wambu <jessewambu@zetech.ac.ke>
2025-09-27 11:44:45 +0300

    Updated MainActivity.kt

--- app/src/main/java/com/example/mapsai/MainActivity.kt
+++ app/src/main/java/com/example/mapsai/MainActivity.kt
@@ -1,25 +1,25 @@
-package com.example.mapsai
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
 import androidx.navigation.compose.rememberNavController
-import com.example.mapsai.navigation.AppNavGraph
-import com.example.mapsai.ui.theme.MapsAITheme
+import com.example.mapsai.navigation.NavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MapsAITheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    val navController = rememberNavController()
-                    AppNavGraph(navController = navController, modifier = Modifier)
-                }
-            }
+            MyApp()
         }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val navController = rememberNavController()
+    Surface(color = MaterialTheme.colorScheme.background) {
+        NavGraph(navController = navController)
     }
 }
