15c5e168ca4eeb1c3fd98fb4f2ab7cba5fa94810
Jesse Wambu <jessewambu@zetech.ac.ke>
2025-11-11 22:38:28 +0300

    Updated app Files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.example.mapsai"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
